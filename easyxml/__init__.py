c15a7ab9a9f9a3cb4db3ddf3199d11df866ce406
Josh Reed <jriddy@gmail.com>
2017-01-25 17:44:09 -0600

    basic skeleton

---
+++ easyxml/__init__.py
@@ -0,0 +1,6 @@
+from easyxml.converter import EasyXmlConverter
+
+the_converter = EasyXmlConverter()
+
+elements = the_converter.elements
+dump = the_converter.dump
---
+++ easyxml/converter.py
@@ -0,0 +1,11 @@
+"""Converts a iterable and dict-based structure to element trees.
+"""
+
+
+class EasyXmlConverter(object):
+
+    def elements(self, node):
+        pass
+
+    def dump(self, node):
+        pass
