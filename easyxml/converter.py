fffdaf46d0d6ffd6b9fcacb5bf893bef03b23bd6
Josh Reed <jriddy@gmail.com>
2017-01-25 18:17:39 -0600

    convert list elements to etree

--- easyxml/converter.py
+++ easyxml/converter.py
@@ -1,11 +1,26 @@
 """Converts a iterable and dict-based structure to element trees.
 """
+from xml.etree import ElementTree as ET
+from itertools import chain
 
 
 class EasyXmlConverter(object):
 
     def elements(self, node):
-        pass
+        it = iter(node)
+        name = next(it)
+        try:
+            maybe_attribs = next(it)
+        except StopIteration:
+            attribs = {}
+        else:
+            # TODO: improve this check
+            if isinstance(maybe_attribs, dict):
+                attribs = maybe_attribs
+                children = chain((maybe_attribs,), it)
+            else:
+                children = it
+        return ET.Element(name, attribs)
 
     def dump(self, node):
-        pass
+        return ET.tostring(self.elements(node))
